5e2eb83c950f7955e3a90f09ac904e86b9badb59
alanabrit0 <alanaescobarbrito10@gmail.com>
2026-09-09 20:38:04 -0300

    Criação inicial do projeto Angular

---
+++ src/app/components/details/details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-details',
+  styleUrl: './details.css',
+  templateUrl: './details.html',
+})
+export class Details {}
